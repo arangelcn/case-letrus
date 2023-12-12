94b6bf9f4fa368e1ac5ba34d1ca3d71cfbfb50e3
Antonio Rangel <antoniorangel@Antonios-MacBook-Pro.local>
2023-12-12 03:27:57 -0300

    fix: github workflow

--- main.tf
+++ main.tf
@@ -10,8 +10,14 @@ terraform {
 
 }
 
+variable "AWS_ACCESS_KEY_ID" { type= string } 
+variable AWS_ACCESS_KEY_SECRET { type= string } 
+
 provider "aws" {
   region = "ap-southeast-1"
+
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_ACCESS_KEY_SECRET
 
   # set default tags to terraform managed resources
   default_tags {
